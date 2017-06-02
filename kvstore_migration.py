2b271c9933a6950ccfa5fca88401f6d050c6b643
Trusted Subject <trustedsubject@gmail.com>
2017-06-02 02:37:55 +0000

    Various improvements and bug fixes.

--- kvstore_migration.py
+++ kvstore_migration.py
@@ -14,36 +14,63 @@ sport = '8089'
 dport = '8089'
 suser = 'admin'
 duser = 'admin'
-spassword = 'changeme'
-dpassword = 'changeme'
+spassword = ''
+dpassword = ''
 
 # get list of apps
-apps_url = ''.join(['https://',shost,':',sport,'/services/apps/local?output_mode=json'])
+apps_url = ''.join(['https://',shost,':',sport,'/servicesNS/nobody?output_mode=json'])
 apps_r = requests.get(apps_url,auth=(suser,spassword),verify=False)
 apps = json.loads(apps_r.text)
 
 # for each app
 for app in apps['entry']:
+
+    print "Looking for collections in app: " + app['name']
+
     # get a list of collections
     collections_url = ''.join(['https://',shost,':',sport,'/servicesNS/nobody/',app['name'],'/storage/collections/config?output_mode=json'])
     collections_r = requests.get(collections_url,auth=(suser,spassword),verify=False)
     collections = json.loads(collections_r.text)
-    try:
-        # for each collection
-        for collection in collections['entry']:
+
+    for collection in collections['entry']:
+
+        # if the collection belongs to the app
+        if collection['acl']['app'] == app['name']:
+
+            print "Found collection: " + collection['name']
+        
             # get the collection's records
             scollection_url = ''.join(['https://',shost,':',sport,'/servicesNS/nobody/',app['name'],'/storage/collections/data/',collection['name']])
             scollection_r = requests.get(scollection_url,auth=(suser,spassword),verify=False)
-            # if records were returned
-            if scollection_r.status_code == 200:
-                print app['name'] + " " + collection['name']
-                # populate the destination collection
-                dcollection_url = ''.join(['https://',dhost,':',dport,'/servicesNS/nobody/',app['name'],'/storage/collections/data/',collection['name'],'/batch_save'])
+        
+            # if the collection isn't empty
+            if scollection_r.text != '[ ]':
+
+                collection_json = json.loads(scollection_r.text)
+                collection_size = len(collection_json)
+                print "Collection size: " + str(collection_size)
                 dcollection_headers = {'Content-type': 'application/json'}
-                try:
+                dcollection_url = ''.join(['https://',dhost,':',dport,'/servicesNS/nobody/',app['name'],'/storage/collections/data/',collection['name'],'/batch_save'])
+
+                if collection_size < 1000:
+                    # populate the destination collection
                     dcollection_r = requests.post(dcollection_url,auth=(duser,dpassword),verify=False,headers=dcollection_headers,data=scollection_r.text)
-                    print dcollection_r.status_code
-                except:
-                    print dcollection_r.status_code
-    except:
-        continue
+                    if dcollection_r.status_code != 200:
+                        print dcollection_r.text
+                else:
+                    start_record = 0
+                    end_record = 1000
+                    passes = (len(collection_json)/1000)+1
+                    print "Performing " + str(passes) + " passes"
+                    p = 1
+                    while p <= passes:
+                        data = json.dumps(collection_json[start_record:end_record])
+                        dcollection_r = requests.post(dcollection_url,auth=(duser,dpassword),verify=False,headers=dcollection_headers,data=data)
+                        if dcollection_r.status_code != 200:
+                            print dcollection_r.text
+                            p = passes + 1
+                        start_record = end_record
+                        end_record += 1000
+                        p += 1
+            else:
+                print "Collection is empty"
