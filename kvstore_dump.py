5aa0616f120a7d89f63d3aba949c97a65e7b429d
Trusted Subject <trustedsubject@gmail.com>
2017-06-07 09:58:16 +1000

    Minor refinements to kvstore_dump.py

--- kvstore_dump.py
+++ kvstore_dump.py
@@ -9,13 +9,9 @@ requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 # Thanks https://github.com/georgestarcher/Splunk-createkvstore/blob/master/makekvstore.py for providing an example that helped me to create this tool
 
 shost = ''
-dhost = ''
 sport = '8089'
-dport = '8089'
 suser = 'admin'
-duser = 'admin'
 spassword = ''
-dpassword = ''
 
 # get list of apps
 apps_url = ''.join(['https://',shost,':',sport,'/servicesNS/nobody?output_mode=json'])
@@ -39,7 +35,6 @@ for app in apps['entry']:
             scollection_url = ''.join(['https://',shost,':',sport,'/servicesNS/nobody/',app['name'],'/storage/collections/data/',collection['name']])
             scollection_r = requests.get(scollection_url,auth=(suser,spassword),verify=False)
         
-            # if the collection isn't empty
-            if scollection_r.text != '[ ]':
-                print "app=" + app['name'] + " collection=" + collection['name']
-                print scollection_r.text
+            print "app=" + app['name'] + " collection=" + collection['name']
+            print scollection_r.text
+            print
